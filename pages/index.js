0bc440e47a097545efb788f830f2b11b0435d4bf
Teman Beck <temanbeck@yahoo.com>
2022-05-06 23:33:26 -0400

    Feature: implemented Dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 import { Fragment as Fr } from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Text, useColorMode } from "@chakra-ui/react";
+import { BsSun, BsMoonStarsFill } from 'react-icons/bs';
+
+
+
 
 export default function Home() {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   const disconnectWallet = useDisconnect();
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Box p={12}>
-      {address ? (
-        <Fr>
-          <Button onClick={disconnectWallet}>Disconnect Wallet</Button>
-          <Text>Your address: {address}</Text>
-        </Fr>
-      ) : (
-        <Button onClick={connectWithMetamask}>Connect with Metamask</Button>
-      )}
+    <Box>
+
+      <Box p={8}>
+        {address ? (
+          <Fr>
+            <Button onClick={disconnectWallet}>Disconnect Wallet</Button>
+            <Text>Your address: {address}</Text>
+          </Fr>
+        ) : (
+          <Button onClick={connectWithMetamask}>Connect with Metamask</Button>
+        )}
+
+        <Button onClick={toggleColorMode}>
+          {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun /> }
+        </Button>
+      </Box>
+      <Box pl={8}>
+        Main Content Area
+      </Box>
     </Box>
   );
 }
