590f888e5df9917d5f1948a0a617cfc8cbca4a6a
Ej Matugas <franzelijahmatugas@gmail.com>
2022-05-12 20:00:25 -0700

    fixed some routes

--- pages/api/collections/index.js
+++ pages/api/collections/index.js
@@ -10,21 +10,23 @@ const createCollection = wrapAsync(async (req, res) => {
     bannerImg,
     ...(!!description && description),
   });
-  res.status(201).json({ status: "success", data: newCollection });
+  return res.status(201).json({ status: "success", data: newCollection });
 });
 
 const getAllCollections = wrapAsync(async (req, res) => {
   const collections = await Collections.findAll();
-  res.status(200).json({ status: "success", data: collections });
+  return res.status(200).json({ status: "success", data: collections });
 });
 
 export default async function handler(req, res) {
   try {
     switch (req.method) {
       case "GET":
-        getAllCollections(req, res);
+        return await getAllCollections(req, res);
       case "POST":
-        createCollection(req, res);
+        return await createCollection(req, res);
+      default:
+        throw new Error("Not a route");
     }
   } catch (err) {
     res.status(400).json({ status: "error", message: err.message });
