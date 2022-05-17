d617c426bd01d08e0730198b4c0e77512952ba29
Teman Beck <temanbeck@yahoo.com>
2022-05-16 22:11:05 -0400

    Route: /marketplace/nfts index updated

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1,54 +1,53 @@
-import React, { useEffect, useState, Fragment as Fr } from "react";
-import { Box, Heading } from "@chakra-ui/react";
-import NFTList from "../components/marketplace/NFTList";
-import NFTItem from "../components/marketplace/NFTItem";
-import { useAddress, useMarketplace } from "@thirdweb-dev/react";
-import { useRouter } from "next/router";
-import { Flex } from "@chakra-ui/react";
-import { useSelector, useDispatch } from "react-redux";
+// import React, { useEffect, useState, Fragment as Fr } from "react";
+// import { Box, Heading, Flex } from "@chakra-ui/react";
+// import NFTList from "../components/marketplace/NFTList";
+// import NFTItem from "../components/marketplace/NFTItem";
+// import { useAddress, useMarketplace } from "@thirdweb-dev/react";
+// import { useRouter } from "next/router";
+// import { useSelector, useDispatch } from "react-redux";
 
-const Marketplace = () => {
-  const { activeNfts } = useSelector((store) => store);
-  const [isLoading, setIsLoading] = useState(true);
+// const Marketplace = () => {
+//   const { activeNfts } = useSelector((store) => store);
+//   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setIsLoading(false);
-  },[]);
+//   useEffect(() => {
+//     setIsLoading(false);
+//   },[]);
 
-  return (
-    <Box px="12">
-      <Heading textAlign="center" mb="8">
-        Cryptozon Market
-      </Heading>
-      {isLoading ? (
-        <div>Loading</div>
-      ) : (
-        <Flex gap="8" wrap="wrap" justifyContent={"center"}>
-          {activeNfts.activeNfts.length > 0 ? (
-            activeNfts.activeNfts[0].map((nft) => {
-              console.log("nft", nft);
-              const { name, description, image } = nft.asset;
-              const { id, buyoutPrice, tokenId } = nft;
-              const price = buyoutPrice / 1e18;
+//   return (
+//     <Box px="12">
+//       <Heading textAlign="center" mb="8">
+//         Cryptozon Market
+//       </Heading>
+//       {isLoading ? (
+//         <div>Loading</div>
+//       ) : (
+//         <Flex gap="8" wrap="wrap" justifyContent={"center"}>
+//           {activeNfts.activeNfts.length > 0 ? (
+//             activeNfts.activeNfts[0].map((nft) => {
+//               console.log("nft", nft);
+//               const { name, description, image } = nft.asset;
+//               const { id, buyoutPrice, tokenId } = nft;
+//               const price = buyoutPrice / 1e18;
 
-              return (
-                <NFTItem
-                  key={tokenId}
-                  name={name}
-                  description={description}
-                  image={image}
-                  price={price}
-                  id={id}
-                />
-              );
-            })
-          ) : (
-            <div>No listings</div>
-          )}
-        </Flex>
-      )}
-    </Box>
-  );
-};
+//               return (
+//                 <NFTItem
+//                   key={tokenId}
+//                   name={name}
+//                   description={description}
+//                   image={image}
+//                   price={price}
+//                   id={id}
+//                 />
+//               );
+//             })
+//           ) : (
+//             <div>No listings</div>
+//           )}
+//         </Flex>
+//       )}
+//     </Box>
+//   );
+// };
 
-export default Marketplace;
+// export default Marketplace;
