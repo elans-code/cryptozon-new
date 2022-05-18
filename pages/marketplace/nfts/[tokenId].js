758372359cb35a6e0d38c584e39f231f368cac89
Teman Beck <temanbeck@yahoo.com>
2022-05-18 07:00:03 -0400

    Functionality: implemented ability to display single NFT

--- pages/marketplace/nfts/[tokenId].js
+++ pages/marketplace/nfts/[tokenId].js
@@ -2,9 +2,9 @@
 //useRouter hook next/router
 //will use same NFTActiveItem component for single NFT display
 //tokenId is the id of the NFT
-
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import NFTActiveItem from "../../../components/marketplace/NFTActiveItem";
+import NFTSingleItem from "../../../components/marketplace/NFTSingleItem";
 
 // export const getStaticProps = async ({ params }) => {
 //   const { tokenId } = params;
@@ -23,21 +23,35 @@ const WINDOW_URL = window.location.href;
 const NFTDetails = () => {
   const { activeNfts } = useSelector((store) => store);
   const activeNow = activeNfts.activeNfts[0];
-
   const singleNFTData = activeNow.filter((nft) => {
     const testURL = URL_PREFIX + nft.tokenId
-    if (WINDOW_URL === testURL) {
+    if (WINDOW_URL == testURL) {
+      console.log(nft);
       return nft;
     }
   })
-  console.log(singleNFTData[0].asset);
+  console.log(singleNFTData[0]);
+  const { name, description, image } = singleNFTData[0].asset;
+  const { id, buyoutPrice, tokenId } = singleNFTData[0];
+  const price = buyoutPrice / 1e18;
+
   return (
-    <div>
-      <h1>
-        Details for NFT with tokenId:
-      </h1>
-    </div>
+    <NFTSingleItem
+      key={tokenId}
+      name={name}
+      description={description}
+      image={image}
+      price={price}
+      id={id}
+      tokenId={tokenId}
+    />
   )
+
+  // return (
+  //   <div>
+  //     Okay
+  //   </div>
+  // )
 }
 
 
