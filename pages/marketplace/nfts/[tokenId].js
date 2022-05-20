0f96671898b24fb0d39809de7d0c045edca41ba0
spkillaz <elan.savinon@gmail.com>
2022-05-20 14:22:47 -0400

    test

--- pages/marketplace/nfts/[tokenId].js
+++ pages/marketplace/nfts/[tokenId].js
@@ -22,7 +22,10 @@ import NFTSingleItem from "../../../components/marketplace/NFTSingleItem";
 const NFTDetails = () => {
   const { activeNfts } = useSelector((store) => store);
   const activeNow = activeNfts.activeNfts[0];
-  const singleNFTData = activeNow.filter((nft) => {
+  const singleNFTData = []
+  if(!!activeNow)
+  {
+    singleNFTData = activeNow.filter((nft) => {
     const URL_PREFIX = "http://localhost:3000/marketplace/nfts/";
     const WINDOW_URL = window.location.href;
     const testURL = URL_PREFIX + nft.tokenId
@@ -30,23 +33,27 @@ const NFTDetails = () => {
       // console.log(nft);
       return nft;
     }
-  })
-
-  const { name, description, image } = singleNFTData[0].asset;
-  const { id, buyoutPrice, tokenId } = singleNFTData[0];
-  const price = buyoutPrice / 1e18;
-
-  return (
-    <NFTSingleItem
-      key={tokenId}
-      name={name}
-      description={description}
-      image={image}
-      price={price}
-      id={id}
-      tokenId={tokenId}
-    />
-  )
+  })}
+  console.log('singlenftdata: ',singleNFTData[0])
+  if(!!singleNFTData[0]){
+    const { name, description, image } = singleNFTData[0].asset;
+    const { id, buyoutPrice, tokenId } = singleNFTData[0];
+    const price = buyoutPrice / 1e18;
+    return (
+      <NFTSingleItem
+        key={tokenId}
+        name={name}
+        description={description}
+        image={image}
+        price={price}
+        id={id}
+        tokenId={tokenId}
+      />
+    )
+  }
+  else{
+    return (null)
+  }
 
   // return (
   //   <div>
